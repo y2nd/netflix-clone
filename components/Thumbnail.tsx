ea568afc89e7fa8b2aaceb211e6725eaae8dfc14
Youssef T <sheese.711@gmail.com>
2022-07-19 14:27:07 +0200

    got rid of z-index on thumbnails

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -7,19 +7,22 @@ interface Props {
 }
 const Thumbnail = ({ movie }: Props) => {
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">   
-        <Image
-        src={`${BASE_URL_IMAGE}${
-            movie.backdrop_path || movie.poster_path
-        }`}
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+      <Image
+        src={`${BASE_URL_IMAGE}${movie.backdrop_path || movie.poster_path}`}
         alt={movie.name}
         layout="fill"
         className="rounded-sm object-cover md:rounded "
-        />
+      />
 
-        <div className="w-full h-full flex items-center justify-center bg-gray-500 transition duration-300 opacity-0 hover:opacity-80 rounded-sm">
-            <h2>{movie.original_name || movie.name || movie.title || movie.original_title}</h2>
-        </div>
+      <div className="w-full h-full flex items-center justify-center bg-gray-500 transition duration-300 opacity-0 hover:opacity-80 rounded-sm">
+        <h2>
+          {movie.original_name ||
+            movie.name ||
+            movie.title ||
+            movie.original_title}
+        </h2>
+      </div>
     </div>
   );
 };
