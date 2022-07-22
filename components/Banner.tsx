68cb39dc01bfea7ca4e8e4a6ac1c2e613e2752b2
y2nd <sheese.711@gmail.com>
2022-07-23 00:16:01 +0200

    fetching data for the movie banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,6 +4,8 @@ import { BASE_URL } from "../constants/movie";
 import { Movie } from "../typings";
 import { FaPlay } from "react-icons/fa";
 import { InformationCircleIcon } from "@heroicons/react/solid";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -11,6 +13,8 @@ interface Props {
 
 const Banner = ({ netflixOriginals }: Props) => {
   const [movie, setMovie] = useState<Movie | null>(null);
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
 
   useEffect(() => {
     setMovie(
@@ -36,8 +40,18 @@ const Banner = ({ netflixOriginals }: Props) => {
       </p>
 
       <div className="flex space-x-3">
-        <button className="banner-button bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play</button>
-        <button className="banner-button bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/></button>
+        <button className="banner-button bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button
+          className="banner-button bg-[gray]/70"
+          onClick={() => {
+            setShowModal(true);
+            setCurrentMovie(movie);
+          }}
+        >
+          More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
       </div>
     </div>
   );
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import MuiModal from "@mui/material/Modal";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
+import { XIcon } from "@heroicons/react/solid";
+import { useEffect, useState } from "react";
+import { Movie } from "../typings";
 
 const Modal = () => {
-  return (
-    <div>Modal</div>
-  )
-}
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [movie, setMovie] = useRecoilState(movieState);
+  
 
-export default Modal
+  useEffect(() => {
+    if (!movie) return;
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === "tv" ? "tv" : "movie"
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      ).then((response) => response.json());
+    }  
+    fetchMovie();
+  }, [movie]);
+
+  const handleClose = () => {
+    setShowModal(false);
+    setMovie(null);
+  };
+  return (
+    <MuiModal open={showModal} onClose={handleClose}>
+      <>
+        <button
+          onClick={handleClose}
+          className="modal-button absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+        >
+          <XIcon className="h-6 w-6" />
+        </button>
+
+        <div className=""></div>
+      </>
+    </MuiModal>
+  );
+};
+
+export default Modal;
