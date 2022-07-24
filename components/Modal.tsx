29d197c8eb04e62484b53d745302d41ab95724f7
y2nd <sheese.711@gmail.com>
2022-07-24 15:10:36 +0200

    added match percentage and release date under the trailer

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -96,6 +96,16 @@ const Modal = () => {
             </button>
           </div>
         </div>
+
+        <div className="">
+          <div className="">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">{movie?.vote_average*10}% Match</p>
+              <p className="font-light">{movie?.release_date || movie?.first_air_date}</p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">HD</div> 
+            </div>
+          </div>
+        </div>
       </>
     </MuiModal>
   );
