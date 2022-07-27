b50bc5a611427635fbf42b272af537cefe4e931f
Youssef T <sheese.711@gmail.com>
2022-07-27 13:32:33 +0200

    subcription Table component

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -1,17 +1,18 @@
 import { CheckIcon } from "@heroicons/react/solid";
+import Table from "../components/Table";
 import { Product } from "@stripe/firestore-stripe-payments";
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import useAuth from "../hooks/useAuth";
-
 
 interface Props {
   products: Product[];
 }
 
-const Plans = ( {products}: Props) => {
+const Plans = ({ products }: Props) => {
   const { logout } = useAuth();
+  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2]);
 
   const iconStyles = "h-7 w-7 text-[#E50914]";
   const liStyles = "flex items-center gap-x-2 text-lg";
@@ -38,7 +39,7 @@ const Plans = ( {products}: Props) => {
         </button>
       </header>
 
-      <main className="pt-28">
+      <main className="mx-auto max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10">
         <h1 className="mb-3 text-3xl font-medium">
           Choose the plan that's right for you
         </h1>
@@ -57,14 +58,22 @@ const Plans = ( {products}: Props) => {
 
         <div className="mt-4 flex flex-col space-y-4">
           <div className="flex w-full items-center justify-end self-end md:w-3/5">
-            <div className="plan-box">standard</div>
-            <div className="plan-box">standard</div>
-            <div className="plan-box">standard</div>
+            {products.map( (product) => (
+              <div
+              className={`plan-box cursor-pointer ${
+                selectedPlan?.id === product.id ? 'opacity-100' : 'opacity-60'
+              }`}
+              key={product.id}
+              onClick={() => setSelectedPlan(product)}
+              
+            >
+              {product.name}
+            </div>
+            ))}
           </div>
 
-          {/* <Table /> */}
+          <Table products={products} selectedPlan={selectedPlan} />
           <button>Susbcribe</button>
-          
         </div>
       </main>
     </div>
