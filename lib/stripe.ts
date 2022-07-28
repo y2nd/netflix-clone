98b383ddd5a0b19c28c65abaad29342a3b530d59
Youssef T <sheese.711@gmail.com>
2022-07-28 16:55:28 +0200

    added goToBillingPortal

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,20 +5,29 @@ import {
 import { getFunctions, httpsCallable } from "@firebase/functions";
 import app from "../firebase";
 
-
 const payments = getStripePayments(app, {
-    productsCollection: "products",
-    customersCollection: "customers"
+  productsCollection: "products",
+  customersCollection: "customers",
 });
 
 const loadCheckout = async (priceId: string) => {
-    await createCheckoutSession(payments, {
-        price: priceId,
-        success_url: window.location.origin,
-        cancel_url: window.location.origin
-    }).then( (snapshot) => window.location.assign(snapshot.url)).catch((error) => console.log(error));
+  await createCheckoutSession(payments, {
+    price: priceId,
+    success_url: window.location.origin,
+    cancel_url: window.location.origin,
+  })
+    .then((snapshot) => window.location.assign(snapshot.url))
+    .catch((error) => console.log(error));
 };
+
+const goToBillingPortal = async () => {
+    const instance = getFunctions(app, "europe-west");
+    const functionRef = httpsCallable(instance, "ext-firestore-stripe-payments-createPortalLink");
+
+    await functionRef({
+        returnUrl: `${window.location.origin}/account`
+    }).then( ({data}: any) => window.location.assign(data.url)).catch((error) => console.log(error.message));
+}
 
 export { loadCheckout };
 export default payments;
-
