48ab3e1eff627aa73ebd9cdc39855faceb83dea1
Youssef T <sheese.711@gmail.com>
2022-07-29 10:17:57 +0200

    cancel subscription

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,3 +1,4 @@
+import {useRouter} from "next/router";
 import { useState } from "react";
 import useAuth from "../hooks/useAuth";
 import useSubscription from "../hooks/useSubscription";
@@ -5,17 +6,16 @@ import { goToBillingPortal } from "../lib/stripe";
 import Loader from "./Loader";
 
 const Membership = () => {
-  const [isBillingLoading, setIsBillingLoading] = useState<boolean>(false);
+  const [isBillingLoading, setBillingLoading] = useState<boolean>(false);
   const { user } = useAuth();
   const subscription = useSubscription(user);
 
-  
+  const router = useRouter();
+
   const manageSubscription = () => {
-    if(subscription) {
-        console.log(subscription);
-        setIsBillingLoading(true);
-        goToBillingPortal();
-        setIsBillingLoading(false);
+    if (subscription) {
+      setBillingLoading(true);
+      goToBillingPortal();
     }
   };
 
@@ -23,17 +23,19 @@ const Membership = () => {
     <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
       <div className="space-y-2 py-2">
         <h4 className="text-lg text-[gray]">Membership & Billing</h4>
-        <button
-          className="bg-gray-300 text-black h-10 w-3/5 whitespace-nowrap py-2 text-sm font-medium shadow-md hover:bg-gray-200 md:w-4/5 rounded"
-          disabled={isBillingLoading || !subscription}
-          onClick={manageSubscription}
-        >
-          {isBillingLoading ? (
-            <Loader color="dark:fill-[#e50914]" />
-          ) : (
-            `Cancel Membership`
-          )}
-        </button>
+        {subscription ? (
+          <button
+            className="bg-gray-300 text-black h-10 w-3/5 whitespace-nowrap py-2 text-sm font-medium shadow-md hover:bg-gray-200 md:w-4/5 rounded"
+            disabled={isBillingLoading || !subscription}
+            onClick={manageSubscription}
+          >
+            {isBillingLoading ? (
+              <Loader color="dark:fill-[#e50914]" />
+            ) : (
+              "Cancel Membership"
+            )}
+          </button>
+        ) : <button className="bg-gray-300 text-black h-10 w-3/5 whitespace-nowrap py-2 text-sm font-medium shadow-md hover:bg-gray-200 md:w-4/5 rounded" onClick={() => router.push('/')}>Subscribe to a plan</button>}
       </div>
     </div>
   );
--- lib/stripe.ts
+++ lib/stripe.ts
@@ -21,13 +21,17 @@ const loadCheckout = async (priceId: string) => {
 };
 
 const goToBillingPortal = async () => {
-    const instance = getFunctions(app, "europe-west");
-    const functionRef = httpsCallable(instance, "ext-firestore-stripe-payments-createPortalLink");
+  const instance = getFunctions(app, 'europe-west1');
+  const functionRef = httpsCallable(
+    instance,
+    'ext-firestore-stripe-payments-createPortalLink'
+  );
 
-    await functionRef({
-        returnUrl: `${window.location.origin}/account`
-    }).then( ({data}: any) => window.location.assign(data.url)).catch((error) => console.log(error.message));
+  await functionRef({
+    returnUrl: `${window.location.origin}/account`,
+  })
+    .then(({ data }: any) => window.location.assign(data.url))
+    .catch((error) => console.log(error.message))
 }
-
 export { loadCheckout, goToBillingPortal };
 export default payments;
